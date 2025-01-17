1abe32130677daa462b5507ce2b17558d9abdf26
Tolrom <max.le.morlot@gmail.com>
2025-01-17 18:41:26 +0100

    Setup global + home page + design boutons

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './views/public/home/home.component';
+import { NotFoundComponent } from './views/public/notFound/NotFound.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '', 
+        redirectTo: 'home', 
+        pathMatch: 'full',
+        title: 'Kura',
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        // canActivateChild: [reverseAuthGuard],
+        // children: [
+        //     {
+        //         path: '', 
+        //         redirectTo: 'login', 
+        //         pathMatch: 'prefix',
+        //     },
+        //     {
+        //         path: 'register', 
+        //         // component: RegisterComponent,
+        //         title: 'Inscription | NuHo',
+        //     },
+        //     {
+        //         path: 'login', 
+        //         // component: LoginComponent,
+        //         title: 'Connexion | NuHo',
+        //     },
+        //     {
+        //         path: 'pass-reset',
+        //         // component: PassResetComponent,
+        //         title: 'Mot de passe oublié | NuHo'
+        //     },
+        //     {
+        //         path: 'confirm/:token',
+        //         // component: ConfirmationComponent,
+        //         title: 'Confirmation de l\'email | NuHo'
+        //     },
+        // ]
+    },
+    
+    {
+        path: '**',
+        component: NotFoundComponent,
+        title: '404 Not Found',
+    },
+];
---
+++ apps/frontend/src/app/views/public/notFound/NotFound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [CommonModule],
+  templateUrl: './NotFound.component.html',
+  styleUrl: './NotFound.component.css',
+})
+export class NotFoundComponent {}
